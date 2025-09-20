c1d4be81f887be42533db7d7f0913859cb4c7034
Enzey <github@enzey.net>
2025-09-19 21:49:00 -0400

    folder structure change

--- code/backend/src/main.ts
+++ code/backend/src/main.ts
@@ -1,100 +1,15 @@
 import {default as fs} from 'fs';
-import {default as express} from 'express';
-import {
-    getLightsOnNetwork as getLightsOnNetwork,
-} from './scanForLights.ts';
-import {
-    ElgatoLight as ElgatoLight,
-    ElgatoDevice as ElgatoDevice,
-    setDeviceName as setDeviceName,
-    setLight as setLight
-} from './api.ts';
+import express from 'express';
 
-import {
-    getLights as getLights,
-    getDevicesInGroup as getDevicesInGroup,
-    addDeviceToGroup as addDeviceToGroup,
-    updateActiveDevices as updateActiveDevices
-} from './deviceManager.ts'
-
-async function scanAndUpdateCache() {
-    try {
-        const deviceIPs = await getLightsOnNetwork();
-        await updateActiveDevices(deviceIPs);
-    } catch (e) {
-        console.log(e)
-    }
-}
+import services from './servicesRouter';
 
 const app = express();
 app.use(express.json());
 
-app.post('/force', async (req, res) => {
-    let appReturn;
-    try {
-        await scanAndUpdateCache();
-        res.send();
-    } catch (e) {
-        res.status(500).send();
-        console.log(e)
-    }
+app.use('/api', services)
 
-    res.send(appReturn);
-});
-
-app.put('/light', async (req, res) => {
-    try {
-        const data: ElgatoLight = req.body.light;
-        if (!data) {
-            res.status(422).send();
-            return
-        }
-
-        let lights: ElgatoDevice[] | null = null;
-        if (req.body.group) {
-            lights = getDevicesInGroup(req.body.group)
-        }
-        if (req.body.serialNumber) {
-            lights = getLights().filter(light => light.serialNumber === req.body.serialNumber);
-        }
-
-        if (!lights) {
-            res.status(422).send();
-            return
-        }
-
-        for (const light of lights) {
-            await setLight(light.ip, data.brightness, data.temperature)
-        }
-
-        res.send();
-    } catch (e) {
-        res.status(422).send(e);
-    }
-});
-
-app.put('/group', async (req, res) => {
-    try {
-        const data = req.body;
-
-        const groupName: String = data.groupName
-        const serialNumbers: String[] = data.serialNumbers
-        for (const serialNumber of serialNumbers) {
-            addDeviceToGroup(groupName, serialNumber)
-        }
-        
-        res.send();
-    } catch (e) {
-        res.status(422).send(e);
-    }
-});
+app.use(express.static('dist/web'))
 
 app.listen(8080, () => {
     console.log(`Server is listening`);
 });
-
-async function start() {
-    await scanAndUpdateCache();
-    console.log(getLights())
-}
-start()
